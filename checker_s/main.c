e69c74779a3cf9a61512d10edafab8348707221a
Artem Kalmykov <kalmykov.artem@gmail.com>
2017-01-10 21:29:06 -0800

    git git

--- checker_s/main.c
+++ checker_s/main.c
@@ -10,11 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft/libft.h"
+/*
+ * write
+ * read
+ * malloc
+ * free
+ * exit
+ */
 
-int		main(void)
+#include "../shared_s/push_swap.h"
+
+static void		ft_isinorder(int *tab, int size)
 {
-	ft_printf("ckecher main\n");
-	return (0);
+	int 		index;
+	int 		number;
+
+	index = 0;
+	number = tab[index];
+	index++;
+	while (index < size)
+	{
+		if (number < tab[index])
+			number = tab[index];
+		else
+		{
+			ft_printf("что тут не так\n");
+			break;
+		}
+		size--;
+	}
 }
 
+static void		ft_poehali(char *str)
+{
+	return ;
+}
+
+int		main(int argc, char **argv)
+{
+	int 	**tab;
+	int 	index;
+
+	if (argc > 2)
+	{
+		tab = (int**)malloc(sizeof(int) * argc);
+		index = 0;
+		while (index < argc)
+		{
+			tab[index] = (int)malloc(sizeof(int)* 1);
+//			tab[index] = (int)ft_atoi(*argv[index]);
+			tab[index] = &index;
+			index++;
+		}
+	}
+	else
+		ft_printf("error\n");
+	return (0);
+}
