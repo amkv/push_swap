0bea7a616e7f85042485d0c869184640fc13ed56
Artem Kalmykov <kalmykov.artem@gmail.com>
2017-01-10 22:14:56 -0800

    progress

--- checker_s/main.c
+++ checker_s/main.c
@@ -11,57 +11,66 @@
 /* ************************************************************************** */
 
 /*
- * write
- * read
- * malloc
- * free
- * exit
+ * write, read, malloc, free, exit
  */
 
 #include "../shared_s/push_swap.h"
+#pragma GCC diagnostic ignored "-Wunused-function"
 
 static void		ft_isinorder(int *tab, int size)
 {
 	int 		index;
 	int 		number;
+	int 		tab_number;
 
+	if (size <= 1)
+	{
+		ft_printf("что-то тут не так\n");
+		return ;
+	}
 	index = 0;
 	number = tab[index];
 	index++;
 	while (index < size)
 	{
-		if (number < tab[index])
-			number = tab[index];
+		tab_number = tab[index];
+		if (number < tab_number)
+			number = tab_number;
 		else
 		{
-			ft_printf("что тут не так\n");
-			break;
+			ft_printf("что-то тут не так\n");
+			return ;
 		}
 		size--;
+		index++;
 	}
+	ft_printf("отсортирован и прекрасен\n");
 }
 
-static void		ft_poehali(char *str)
-{
-	return ;
-}
+//static void		ft_poehali(char *str)
+//{
+//	return ;
+//}
 
 int		main(int argc, char **argv)
 {
-	int 	**tab;
+	int 	*tab;
 	int 	index;
 
+	ft_printf("argc:  %d\n\n", argc);
 	if (argc > 2)
 	{
-		tab = (int**)malloc(sizeof(int) * argc);
 		index = 0;
-		while (index < argc)
+		tab = (int*)malloc(sizeof(int) * argc);
+		while (index < argc - 1)
 		{
-			tab[index] = (int)malloc(sizeof(int)* 1);
-//			tab[index] = (int)ft_atoi(*argv[index]);
-			tab[index] = &index;
+			tab[index] = (int)ft_atoi(argv[index + 1]);
+			ft_printf("index:  %d\n", index);
+			ft_printf("atoi:   %d\n", (int)ft_atoi(argv[index + 1]));
+			ft_printf("tab[i]: %d\n\n", tab[index]);
 			index++;
 		}
+		ft_isinorder(tab, argc);
 	}
 	else
 		ft_printf("error\n");
